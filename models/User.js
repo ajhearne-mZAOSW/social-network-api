7d270b6dff609b70a6b8e791b4b2cd43e38d0c1d
Alana Hearne <ajhearne@live.com>
2024-10-07 19:44:51 +1100

    starter code, models and schema

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+// schema to create User model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email address']
+        },
+        thoughts: {
+            type: Schema.Types.ObjectId,
+            ref: 'thought',
+        },
+        friends: {
+            type: Schema.Types.ObjectId,
+            ref: 'user',
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+// virtual called friendCount that retrieves the length of the user's friends array field on query
+userSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
